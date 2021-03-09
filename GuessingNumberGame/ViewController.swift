8d62d7939596532d465b0512b9dd1a1acc29f517
廖昱晴 <pulin.liao@ctbcsec.com>
2021-03-09 11:05:46 +0800

    Initial Commit

---
+++ GuessingNumberGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessingNumberGame
+//
+//  Created by 廖昱晴 on 2021/3/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
